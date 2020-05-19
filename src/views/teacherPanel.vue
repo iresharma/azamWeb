c6d789d9e16b4ef1600981662b35f83d081f59fb
Iresharma <iresh.sharma8@gmail.com>
2020-05-20 01:34:23 +0530

    admin panel sidebar done

--- src/views/teacherPanel.vue
+++ src/views/teacherPanel.vue
@@ -2,7 +2,7 @@
     <div style="overflow: hidden" class="columns">
         <div class="column is-2" style="overflow: hidden; background-color: #ffdd57;">
             <ul style="margin: 20px; text-align: left">
-                <li @click="change(0)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
+                <li id="li0" @click="change(0)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
                     <svg class="bi bi-building" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M15.285.089A.5.5 0 0115.5.5v15a.5.5 0 01-.5.5h-3a.5.5 0 01-.5-.5V14h-1v1.5a.5.5 0 01-.5.5H1a.5.5 0 01-.5-.5v-6a.5.5 0 01.418-.493l5.582-.93V3.5a.5.5 0 01.324-.468l8-3a.5.5 0 01.46.057zM7.5 3.846V8.5a.5.5 0 01-.418.493l-5.582.93V15h8v-1.5a.5.5 0 01.5-.5h2a.5.5 0 01.5.5V15h2V1.222l-7 2.624z" clip-rule="evenodd"/>
                         <path fill-rule="evenodd" d="M6.5 15.5v-7h1v7h-1z" clip-rule="evenodd"/>
@@ -10,34 +10,34 @@
                     </svg>
                     Dashboard
                 </li>
-                <li @click="change(1)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
+                <li id="li1" @click="change(1)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
                     <svg class="bi bi-upload" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M.5 8a.5.5 0 01.5.5V12a1 1 0 001 1h12a1 1 0 001-1V8.5a.5.5 0 011 0V12a2 2 0 01-2 2H2a2 2 0 01-2-2V8.5A.5.5 0 01.5 8zM5 4.854a.5.5 0 00.707 0L8 2.56l2.293 2.293A.5.5 0 1011 4.146L8.354 1.5a.5.5 0 00-.708 0L5 4.146a.5.5 0 000 .708z" clip-rule="evenodd"/>
                         <path fill-rule="evenodd" d="M8 2a.5.5 0 01.5.5v8a.5.5 0 01-1 0v-8A.5.5 0 018 2z" clip-rule="evenodd"/>
                     </svg>
                     Upload
                 </li>
-                <li @click="change(2)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
+                <li id="li2" @click="change(2)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
                     <svg class="bi bi-question" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path d="M5.25 6.033h1.32c0-.781.458-1.384 1.36-1.384.685 0 1.313.343 1.313 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.007.463h1.307v-.355c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.326 0-2.786.647-2.754 2.533zm1.562 5.516c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
                     </svg>
                     Quizes
                 </li>
-                <li @click="change(3)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
+                <li id="li3" @click="change(3)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
                     <svg class="bi bi-aspect-ratio" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M0 3.5A1.5 1.5 0 011.5 2h13A1.5 1.5 0 0116 3.5v9a1.5 1.5 0 01-1.5 1.5h-13A1.5 1.5 0 010 12.5v-9zM1.5 3a.5.5 0 00-.5.5v9a.5.5 0 00.5.5h13a.5.5 0 00.5-.5v-9a.5.5 0 00-.5-.5h-13z" clip-rule="evenodd"/>
                         <path fill-rule="evenodd" d="M2 4.5a.5.5 0 01.5-.5h3a.5.5 0 010 1H3v2.5a.5.5 0 01-1 0v-3zm12 7a.5.5 0 01-.5.5h-3a.5.5 0 010-1H13V8.5a.5.5 0 011 0v3z" clip-rule="evenodd"/>
                     </svg>
                     Start Class
                 </li>
-                <li @click="change(4)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
+                <li id="li4" @click="change(4)" class="aim" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
                     <svg class="bi bi-briefcase" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M0 12.5A1.5 1.5 0 001.5 14h13a1.5 1.5 0 001.5-1.5v-6h-1v6a.5.5 0 01-.5.5h-13a.5.5 0 01-.5-.5v-6H0v6z" clip-rule="evenodd"/>
                         <path fill-rule="evenodd" d="M0 4.5A1.5 1.5 0 011.5 3h13A1.5 1.5 0 0116 4.5v2.384l-7.614 2.03a1.5 1.5 0 01-.772 0L0 6.884V4.5zM1.5 4a.5.5 0 00-.5.5v1.616l6.871 1.832a.5.5 0 00.258 0L15 6.116V4.5a.5.5 0 00-.5-.5h-13zM5 2.5A1.5 1.5 0 016.5 1h3A1.5 1.5 0 0111 2.5V3h-1v-.5a.5.5 0 00-.5-.5h-3a.5.5 0 00-.5.5V3H5v-.5z" clip-rule="evenodd"/>
                     </svg>
                     Fee ledger
                 </li>
-                <li @click="change(5)" class="aim" v-if="type == 'admin'" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
+                <li id="li5" @click="change(5)" class="aim" v-if="type == 'admin'" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
                     <svg class="bi bi-chat-quote" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M2.678 11.894a1 1 0 01.287.801 10.97 10.97 0 01-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 01.71-.074A8.06 8.06 0 008 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 01-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 00.244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 01-2.347-.306c-.52.263-1.639.742-3.468 1.105z" clip-rule="evenodd"/>
                         <path d="M7.468 7.667c0 .92-.776 1.666-1.734 1.666S4 8.587 4 7.667C4 6.747 4.776 6 5.734 6s1.734.746 1.734 1.667z"/>
@@ -47,7 +47,7 @@
                     </svg>
                     Contact
                 </li>
-                <li @click="change(6)" class="aim" v-if="type == 'admin'" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
+                <li id="li6" @click="change(6)" class="aim" v-if="type == 'admin'" style="color: black; padding: 5px; margin: 10px 0 10px 0; font-size: 20px">
                     <svg class="bi bi-gear-wide-connected" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M9.928 1.723c-.243-.97-1.62-.97-1.863 0l-.072.286a.96.96 0 01-1.622.435l-.204-.212c-.695-.718-1.889-.03-1.614.932l.08.283a.96.96 0 01-1.186 1.187l-.283-.081c-.961-.275-1.65.919-.932 1.614l.212.204a.96.96 0 01-.435 1.622l-.286.072c-.97.242-.97 1.62 0 1.863l.286.071a.96.96 0 01.435 1.622l-.212.205c-.718.695-.03 1.888.932 1.613l.283-.08a.96.96 0 011.187 1.187l-.081.283c-.275.96.919 1.65 1.614.931l.204-.211a.96.96 0 011.622.434l.072.286c.242.97 1.62.97 1.863 0l.071-.286a.96.96 0 011.622-.434l.205.212c.695.718 1.888.029 1.613-.932l-.08-.283a.96.96 0 011.187-1.188l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205A.96.96 0 0115.983 10l.286-.071c.97-.243.97-1.62 0-1.863l-.286-.072a.96.96 0 01-.434-1.622l.212-.204c.718-.695.029-1.889-.932-1.614l-.283.08a.96.96 0 01-1.188-1.186l.081-.283c.275-.961-.918-1.65-1.613-.932l-.205.212A.96.96 0 0110 2.009l-.071-.286zm-.932 12.27a4.998 4.998 0 100-9.994 4.998 4.998 0 000 9.995z" clip-rule="evenodd"/>
                         <path fill-rule="evenodd" d="M8.372 8.996L5.598 5.298l.8-.6 2.848 3.798h4.748v1H9.246l-2.849 3.798-.8-.6 2.775-3.698z" clip-rule="evenodd"/>
@@ -57,7 +57,7 @@
             </ul>
         </div>
         <div class="column">
-            <upload />
+            <upload v-if="comp == 1" />
         </div>
     </div>
 </template>
@@ -65,9 +65,6 @@
 <style scoped>
 .aim {
     cursor: pointer;
-}
-.active {
-    background-color: #ffcc46;
 }
 
 </style>
@@ -81,7 +78,8 @@ export default {
     data() {
         return {
             logged: '',
-            type: ''
+            type: '',
+            comp: 0
         }
     },
     beforeMount() {
@@ -90,6 +88,15 @@ export default {
     },
     components: {
         upload
+    },
+    methods: {
+        change(ind) {
+            this.comp = ind;
+            for(var i = 0; i < 7; i++) {
+                this.$el.querySelector('#li'+i).style.background = 'transparent';
+            }
+            this.$el.querySelector('#li' + ind).style.background = '#ffcc46';
+        }
     }
 }
 </script>
