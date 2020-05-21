5dde129628dc83d74186224a611add1c9e38b85a
Iresharma <iresh.sharma8@gmail.com>
2020-05-21 21:34:38 +0530

    upload hundred percebt done

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -25,7 +25,9 @@
                             </div>
                             <br>
                             <input class="input" v-model="pdf.title" placeholder="Pdf name" type="text"><br><br>
-                            <input class="input" v-model="pdf.subtitle" placeholder="Pdf description" type="text">
+                            <input class="input" v-model="pdf.subtitle" placeholder="Pdf description" type="text"><br><br>
+                            <strong v-if="fillpdf">Please fill all fields</strong>
+                            
                             <div style="margin: 10px;">
                                 <div style="display: flex; justify-content: space-between">
                                     <big class="muted-text">Progress: {{uploadValuepdf.toFixed()+"%"}}</big>
@@ -33,8 +35,9 @@
                                 </div>
                                 <progress class="progress is-success" :value="uploadValuepdf" max="100" ></progress>  
                             </div>
-
-                            <a @click="onUploadpdf" class="button is-rounded is-warning is-medium">Upload pdf</a>
+                            
+                            <a v-if="pdfNotes == null" class="button is-rounded is-warning is-medium is-loading">Upload pdf</a>
+                            <a v-else @click="onUploadpdf" class="button is-rounded is-warning is-medium">Upload pdf</a>
                     </div>
                 </div> 
             </div>
@@ -65,7 +68,8 @@
                             </div>
                             <br>
                             <input class="input" v-model="video.title" placeholder="Video name" type="text"><br><br>
-                            <input class="input" v-model="video.subtitle" placeholder="Vedio description" type="text">
+                            <input class="input" v-model="video.subtitle" placeholder="Vedio description" type="text"><br><br>
+                            <strong v-if="fillvideo">Please fill all fields</strong>
                             <div style="margin: 10px;">
                                 <div style="display: flex; justify-content: space-between">
                                     <big class="muted-text">Progress: {{uploadValuevideo.toFixed()+"%"}}</big>
@@ -73,8 +77,9 @@
                                 </div>
                                 <progress class="progress is-success" :value="uploadValuevideo" max="100" ></progress>  
                             </div>
-
-                            <a @click="onUploadvideo" class="button is-rounded is-warning is-medium">Upload video</a>
+                            
+                            <a v-if="videoclass == null" class="button is-rounded is-warning is-medium is-loading">Upload pdf</a>
+                            <a v-else @click="onUploadvideo" class="button is-rounded is-warning is-medium">Upload pdf</a>
                     </div>
             </div> 
 
@@ -114,7 +119,10 @@ export default {
       },
       videoclass: null,
       uploadValuepdf: 0,
-      uploadValuevideo: 0
+      uploadValuevideo: 0,
+      load: true,
+      fillpdf: false,
+      fillvideo: false
     }),
     methods:{
         bytesToSize(bytes) {
@@ -125,28 +133,52 @@ export default {
         },
 
         onUploadpdf(){
-            const storageRef=firebaseApp.storageBucket.ref(`pdfs/${this.pdfNotes.name}`).put(this.pdfNotes);
-            storageRef.on(`state_changed`,snapshot=>{
-                this.uploadValuepdf = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
-                }, error=>{console.log(error.message)},
-                ()=>{this.uploadValue=100;
-                    storageRef.snapshot.ref.getDownloadURL().then((url)=>{
-                    this.pdf.src =url;
-                    });
-                }
-            );
+            if(this.pdf.title !== '' && this.pdf.subtitle !== '') {
+                this.fillpdf = false
+                this.pdf.tid = localStorage.getItem('type') == 'admin' ? 'admin' : localStorage.getItem('tid')
+                const storageRef=firebaseApp.storageBucket.ref(`pdfs/${this.pdfNotes.name}`).put(this.pdfNotes);
+                storageRef.on(`state_changed`,snapshot => {
+                    this.uploadValuepdf = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+                    }, 
+                    error => {
+                        console.log(error.message)
+                    },
+                    () => {
+                        this.uploadValue=100;
+                        storageRef.snapshot.ref.getDownloadURL().then((url)=>{
+                            this.pdf.src = url;
+                            firebaseApp.db.collection('pdf').doc().set(this.pdf)
+                        });
+                    }
+                );
+            }
+            else {
+                this.fillpdf = true
+            }
         },
         onUploadvideo(){
-            const storageRef=firebaseApp.storageBucket.ref(`videos/${this.videoclass.name}`).put(this.videoclass);
-            storageRef.on(`state_changed`,snapshot=>{
-                this.uploadValuevideo = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
-                }, error=>{console.log(error.message)},
-                ()=>{this.uploadValue=100;
-                    storageRef.snapshot.ref.getDownloadURL().then((url)=>{
-                        this.video.src = url;
-                    });
-                }
-            );
+            if(this.video.title !== '' && this.video.subtitle !== '') {
+                this.fillvideo = false
+                this.video.tid = localStorage.getItem('type') == 'admin' ? 'admin' : localStorage.getItem('tid')
+                const storageRef=firebaseApp.storageBucket.ref(`videos/${this.videoclass.name}`).put(this.videoclass);
+                storageRef.on(`state_changed`,snapshot => {
+                    this.uploadValuepdf = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+                    }, 
+                    error => {
+                        console.log(error.message)
+                    },
+                    () => {
+                        this.uploadValue=100;
+                        storageRef.snapshot.ref.getDownloadURL().then((url)=>{
+                            this.video.src = url;
+                            firebaseApp.db.collection('video').doc().set(this.video)
+                        });
+                    }
+                );
+            }
+            else {
+                this.fillvideo = true
+            }
         }
 
     }
